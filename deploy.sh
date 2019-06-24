ec1030d4d3a058ea4f34b0968c79b35248b7c0c6
Сергей Гаврыш <gases@krok.local>
2019-06-24 16:24:06 +0300

    code removed from pipeline file to script file

---
+++ deploy.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+scp /home/jenkins/deploy.tar.gz jenkins@ws-ub:/home/jenkins
+ssh jenkins@ws-ub tar -zcvf /home/jenkins/html.tar.gz /var/www/html && rm -rf /var/www/html/*
+ssh jenkins@ws-ub tar -xzvf deploy.tar.gz -C /var/www/
