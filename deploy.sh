9b70d4b214a1230af1e1758f407a36fcef3fa0c5
Сергей Гаврыш <gases@krok.local>
2019-06-25 11:10:03 +0300

    section build

--- deploy.sh
+++ deploy.sh
@@ -2,3 +2,4 @@
 scp /home/jenkins/deploy.tar.gz jenkins@ws-ub:/home/jenkins
 ssh jenkins@ws-ub tar -zcvf /home/jenkins/html.tar.gz /var/www/html && rm -rf /var/www/html/*
 ssh jenkins@ws-ub tar -xzvf deploy.tar.gz -C /var/www/
+rm deploy.tar.gz
